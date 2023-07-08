faba71ebd4f08893e04692be94c5a60ded096e37
Praveen1127 <praveenpandu1127@gmail.com>
2023-07-08 15:41:49 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "movieData.db");
+
+const app = express();
+
+app.use(express.json());
+
+let database = null;
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(4200, () =>
+      console.log("Server Running at http://localhost:4200/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+
+app.get("/movies/", async (request, response) => {
+  const getPlayersQuery = `SELECT movie_name  FROM MOVIE;
+    `;
+  const playersArray = await database.all(getPlayersQuery);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertDbObjectToResponseObject(eachPlayer)
+    )
+  );
+});
